3b596c9ac4a58258f80b77962869f1a84fbb84b1
Stepan Kovalev <65505037+stippo-tech@users.noreply.github.com>
2023-10-30 15:26:36 +0400

    fix: extract heavy widget from home screen

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -28,37 +28,7 @@ class HomeScreen extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Column(
               children: [
-                Container(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: MediaQuery.sizeOf(context).width / 5,
-                    vertical: 20,
-                  ),
-                  child: Column(
-                    children: [
-                      Text(
-                        'Upload excel file. First column should contain keys for dart file with constant strings. '
-                        'Second column should contain keys for json localization files. '
-                        'Values from columns after second will be used in localization files. '
-                        'First value in this columns will be used in json file names. For example for "en" it will "en.json. "'
-                        'Bellow you can see example of an excel file.',
-                        style: Theme.of(context).textTheme.bodyMedium,
-                      ),
-                      const SizedBox(height: 12),
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.black,
-                            width: 2,
-                          ),
-                        ),
-                        child: Image.asset(
-                          'assets/images/example.png',
-                          fit: BoxFit.fitWidth,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+                const _OnboardingWidgets(),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
@@ -118,6 +88,47 @@ class HomeScreen extends StatelessWidget {
             ),
           );
         },
+      ),
+    );
+  }
+}
+
+class _OnboardingWidgets extends StatelessWidget {
+  const _OnboardingWidgets({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: MediaQuery.sizeOf(context).width / 5,
+        vertical: 20,
+      ),
+      child: Column(
+        children: [
+          Text(
+            'Upload excel file. First column should contain keys for dart file with constant strings. '
+            'Second column should contain keys for json localization files. '
+            'Values from columns after second will be used in localization files. '
+            'First value in this columns will be used in json file names. For example for "en" it will "en.json. "'
+            'Bellow you can see example of an excel file.',
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+          const SizedBox(height: 12),
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.black,
+                width: 2,
+              ),
+            ),
+            child: Image.asset(
+              'assets/images/example.png',
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+        ],
       ),
     );
   }
